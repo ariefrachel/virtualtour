1e60e07cdac5af7b206a6937fe27ded6739f7623
Arief Rachman <73397506+ariefrachel@users.noreply.github.com>
2021-04-14 22:17:42 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-var scene, viewer;
-scene = new PANOLENS.ImagePanorama('asset/header.jpg');
-viewer = new PANOLENS.Viewer({
-    controlBar: false,
-    autoRotate: true,
-    autoRotateSpeed: 0.5,
-});
-viewer.add(scene);
